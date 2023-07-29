637fe8d35d6fa544a2298048645a9801ee564edc
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-07-29 12:52:11 -0300

    fix: cors

--- Projeto 3 - DT Money/src/services/api.ts
+++ Projeto 3 - DT Money/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export const api = axios.create({
   baseURL: "https://ignite-react-2022-dtmoney-iqd2ap04q-glsvitoria.vercel.app",
 });
