bc975244505f2fa5da9dd37b4c1e75e552bbe0f4
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-07-29 12:58:41 -0300

    fix: corrs

--- Projeto 3 - DT Money/src/services/api.ts
+++ Projeto 3 - DT Money/src/services/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://ignite-react-2022-dtmoney-iqd2ap04q-glsvitoria.vercel.app",
+  baseURL: "https://ignite-react-2022-dtmoney.vercel.app",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin":
+      "https://ignite-react-2022-dtmoney.vercel.app",
   },
 });
