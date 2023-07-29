9c3d7fc3b6418eeeed2862d927081fd7dac0f9d1
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-07-29 13:01:25 -0300

    feat: change to localhost again

--- Projeto 3 - DT Money/src/services/api.ts
+++ Projeto 3 - DT Money/src/services/api.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://ignite-react-2022-dtmoney.vercel.app",
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin":
-      "https://ignite-react-2022-dtmoney.vercel.app",
-  },
+  baseURL: "http://localhost:3333",
 });
